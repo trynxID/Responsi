63b3f7e7691d016a4604ff5436697d31652783e8
trynxID <sidiq.firman48@gmail.com>
2022-04-21 09:58:25 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4484
+{
+    public class Karyawan
+    {
+        public Karyawan()
+        {
+        }
+        public Karyawan(int nik,string nama, int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            Gajibulanan = gajibulanan;
+        }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int Gajibulanan { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4484
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan();
+            Console.Write("Nik : ");
+            karyawan1.Nik = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nama : ");
+            karyawan1.Nama = Console.ReadLine();
+            Console.Write("Gaji Bulanan : ");
+            karyawan1.Gajibulanan = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Karyawan karyawan2 = new Karyawan();
+            Console.Write("Nik : ");
+            karyawan2.Nik = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nama : ");
+            karyawan2.Nama = Console.ReadLine();
+            Console.Write("Gaji Bulanan : ");
+            karyawan2.Gajibulanan = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("No Nik/Nama          Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("1. {0} ", karyawan1.Nik);
+            Console.Write("{0}", karyawan1.Nama);
+            Console.WriteLine("         {0}", karyawan1.Gajibulanan);
+            Console.Write("2. {0} ", karyawan2.Nik);
+            Console.Write("{0}", karyawan2.Nama);
+            Console.Write("         {0}", karyawan2.Gajibulanan);
+            Console.WriteLine();
+            Console.WriteLine("Asyik Gaji naik 10 %");
+            Console.WriteLine("No Nik/Nama          Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("1. {0} ", karyawan1.Nik);
+            Console.Write("{0}", karyawan1.Nama);
+            Console.WriteLine("         {0}", ((karyawan1.Gajibulanan*0.10)+karyawan1.Gajibulanan));
+            Console.Write("2. {0} ", karyawan2.Nik);
+            Console.Write("{0}", karyawan2.Nama);
+            Console.Write("         {0}", ((karyawan2.Gajibulanan * 0.10) + karyawan2.Gajibulanan));
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
